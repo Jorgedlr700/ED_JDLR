074336ed0b29240ae6da2c832cabb3b39fbaeb3d
Jorgedlr700 <jorge.de3@isepceu.es>
2025-04-30 19:13:39 +0200

    JLDR_InsercionMetodosEnClases

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,35 +12,66 @@ public class Main {
         JDLR_GravedadPlaneta plan3 = new JDLR_GravedadPlaneta(3,"Júpiter", 24.79);
 
         // Calcular pesos de la persona en Tierra
-        double masa1 = pers1.getMasa();
-        double pesoTierra1 = masa1 * plan1.getGravedadPlaneta();
-        double pesoMarte1 = masa1 * plan2.getGravedadPlaneta();
-        double pesoJupiter1 = masa1 * plan3.getGravedadPlaneta();
+        Result result = getResult(pers1, plan1, plan2, plan3);
 
         // Calcular pesos de la persona en Marte
-        double masa2 = pers2.getMasa();
-        double pesoTierra2 = masa2 * plan1.getGravedadPlaneta();
-        double pesoMarte2 = masa2 * plan2.getGravedadPlaneta();
-        double pesoJupiter2 = masa2 * plan3.getGravedadPlaneta();
+        Main.Result2 Result2 = getResult2(pers2, plan1, plan2, plan3);
 
-        // Lo que muestra la  información
+        // Calculo IMC
+        Main.IMC IMC = getImc(pers1, pers2);
+
+        // Lo que muestra la información
         System.out.println(pers1);
-        System.out.println("El IMC de la persona " + pers1.getNombre() + " es " );
+        System.out.println("El IMC de la persona " + pers1.getNombre() + " es: " + IMC.imc1());
 
         System.out.println(pers2);
-        System.out.println("El IMC de la persona " + pers2.getNombre() + " es " );
+        System.out.println("El IMC de la persona " + pers2.getNombre() + " es: " + IMC.imc2());
 
         System.out.println(plan1);
         System.out.println(plan2);
         System.out.println(plan3);
 
-        System.out.println("Peso de " + pers1.getNombre() + " en Tierra: " + pesoTierra1);
-        System.out.println("Peso de " + pers1.getNombre() + " en Marte: " + pesoMarte1);
-        System.out.println("Peso de " + pers1.getNombre() + " en Júpiter: " + pesoJupiter1);
+        System.out.println("Peso de " + pers1.getNombre() + " en Tierra: " + result.pesoTierra1());
+        System.out.println("Peso de " + pers1.getNombre() + " en Marte: " + result.pesoMarte1());
+        System.out.println("Peso de " + pers1.getNombre() + " en Júpiter: " + result.pesoJupiter1());
 
-        System.out.println("Peso de " + pers2.getNombre() + " en Tierra: " + pesoTierra2);
-        System.out.println("Peso de " + pers2.getNombre() + " en Marte: " + pesoMarte2);
-        System.out.println("Peso de " + pers2.getNombre() + " en Júpiter: " + pesoJupiter2);
+        System.out.println("Peso de " + pers2.getNombre() + " en Tierra: " + Result2.pesoTierra2());
+        System.out.println("Peso de " + pers2.getNombre() + " en Marte: " + Result2.pesoMarte2());
+        System.out.println("Peso de " + pers2.getNombre() + " en Júpiter: " + Result2.pesoJupiter2());
+    }
+
+    public static IMC getImc(Persona_jdlr pers1, Persona_jdlr pers2) {
+        double imc1 = pers1.getPeso() / (pers1.getAltura() * pers1.getAltura());
+        double imc2 = pers2.getPeso() / (pers2.getAltura() * pers2.getAltura());
+        IMC IMC = new IMC(imc1, imc2);
+        return IMC;
+    }
+
+   public record IMC(double imc1, double imc2) {
+    }
+
+    private static Result2 getResult2(Persona_jdlr pers2, JDLR_GravedadPlaneta plan1, JDLR_GravedadPlaneta plan2, JDLR_GravedadPlaneta plan3) {
+        double masa2 = pers2.getMasa();
+        double pesoTierra2 = masa2 * plan1.getGravedadPlaneta();
+        double pesoMarte2 = masa2 * plan2.getGravedadPlaneta();
+        double pesoJupiter2 = masa2 * plan3.getGravedadPlaneta();
+        Result2 Result2 = new Result2(pesoTierra2, pesoMarte2, pesoJupiter2);
+        return Result2;
+    }
+
+    private record Result2(double pesoTierra2, double pesoMarte2, double pesoJupiter2) {
+    }
+
+    private static Result getResult(Persona_jdlr pers1, JDLR_GravedadPlaneta plan1, JDLR_GravedadPlaneta plan2, JDLR_GravedadPlaneta plan3) {
+        double masa1 = pers1.getMasa();
+        double pesoTierra1 = masa1 * plan1.getGravedadPlaneta();
+        double pesoMarte1 = masa1 * plan2.getGravedadPlaneta();
+        double pesoJupiter1 = masa1 * plan3.getGravedadPlaneta();
+        Result result = new Result(pesoTierra1, pesoMarte1, pesoJupiter1);
+        return result;
+    }
+
+    private record Result(double pesoTierra1, double pesoMarte1, double pesoJupiter1) {
     }
 }
 
--- src/main/java/org/example/Persona_jdlr.java
+++ src/main/java/org/example/Persona_jdlr.java
@@ -3,9 +3,22 @@ package org.example;
 public class Persona_jdlr {
     private int id;
     private String nombre;
+    private double peso;
     private double  masa;
     private double altura;
     private int edad;
+
+    public Persona_jdlr(double peso) {
+        this.peso = peso;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
 
     public Persona_jdlr() {
     }
@@ -63,9 +76,20 @@ public class Persona_jdlr {
         return "Persona_jdlr{" +
                 "id=" + id +
                 ", nombre='" + nombre + '\'' +
+                ", peso=" + peso +
                 ", masa=" + masa +
                 ", altura=" + altura +
                 ", edad=" + edad +
                 '}';
     }
+    private static Main.IMC getImc(Persona_jdlr pers1, Persona_jdlr pers2) {
+        double imc1 = pers1.getPeso() / (pers1.getAltura() * pers1.getAltura());
+        double imc2 = pers2.getPeso() / (pers2.getAltura() * pers2.getAltura());
+        Main.IMC IMC = new Main.IMC(imc1, imc2);
+        return IMC;
+    }
+
+    private record IMC(double imc1, double imc2) {
+    }
+
 }
